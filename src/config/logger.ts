42fb1527ead7e1fbf6e1e26220cc5e2662a9a411
Anshul Soni <anshulsoni8109@gmail.com>
2026-01-01 21:26:07 +0530

    logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,45 +1,41 @@
-
-
-import winston, { level } from 'winston'
-import { Config } from '.';
+import winston from "winston";
+import { Config } from ".";
 const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     defaultMeta: {
-        serviceName: 'auth-service',
+        serviceName: "auth-service",
     },
     transports: [
         new winston.transports.File({
-            level: 'info',
-            dirname: 'logs',
-            filename: 'combined.log',
+            level: "info",
+            dirname: "logs",
+            filename: "combined.log",
 
-            format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-            silent: Config.NODE_ENV === 'test',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === "test",
         }),
         new winston.transports.File({
-            level: 'error',
-            dirname: 'logs',
-            filename: 'error.log',
+            level: "error",
+            dirname: "logs",
+            filename: "error.log",
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json(),
             ),
-            silent: Config.NODE_ENV === 'test',
-
+            silent: Config.NODE_ENV === "test",
         }),
         new winston.transports.Console({
-            level: 'info',
+            level: "info",
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.json(),
-                
-
             ),
-            silent: Config.NODE_ENV === 'test',
-        })
-
-    ]
-})
-
+            silent: Config.NODE_ENV === "test",
+        }),
+    ],
+});
 
 export default logger;
