795ce74ffca80d9189227ef17fcc39feb4cb9878
Anshul Soni <anshulsoni8109@gmail.com>
2026-01-01 14:43:18 +0530

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
-function welcome(name: string){
-    console.log('Welcome, ' + name);
-
-    
+function welcome(name: string) {
+    console.log("Welcome, " + name);
 }
-welcome("anshul")
+welcome("anshul");
